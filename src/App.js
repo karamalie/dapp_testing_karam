931cb35953affddefab9a4efe697059f4007f710
AhaanSehgal <149166091+AhaanSehgal@users.noreply.github.com>
2023-11-09 14:13:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useTriaConnector } from "@tria-sdk/connect";
 
 function App() {
   const { globalData } = useTriaConnector({ walletUrl: "https://auth-7rin.vercel.app" });
-
   return (
     <div className="bg-black h-[100vh] w-[100vw]">
       <Home/>
