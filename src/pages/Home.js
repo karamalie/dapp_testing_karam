5ae87e84ee25e521bb39486dfd49d20b49dc2678
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2023-11-21 19:24:48 +0000

    contract call update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,23 +31,22 @@ const { data :contractwrite,  write }=useContractWrite({
     chainName: "POLYGON",
     tokenAddress: undefined,
     contractDetails:  {
-      contractAddress: '0x5927Aa58fb36691A4be262c63955b47b67c6e641',
+      contractAddress: '0xd1fD14e3Cf4f96E63A1561681dc8765DF8f7Cf91',
       abi: [
         {
           inputs: [
-            { internalType: 'uint256', name: 'id', type: 'uint256' },
-            { internalType: 'uint256', name: 'amount', type: 'uint256' },
+            { internalType: 'uint256', name: '_tokenID', type: 'uint256' },
+            { internalType: 'address', name: '_claimer', type: 'address' },
           ],
-          name: 'mint',
+          name: 'claimCoupon',
           outputs: [],
-          stateMutability: 'payable',
+          stateMutability: 'nonpayable',
           type: 'function',
         },
       ],
-      functionName: 'mint',
-      //@ts-ignore
-      args: [1, 1],
-      value: 0,
+      functionName: 'claimCoupon',
+      args: [1, '0x7Ae1bBCe3557D46313a960C0982637967eF5c1f7'],
+      // value: 1,
     }});
 
 console.log("send data----------------->",data);
