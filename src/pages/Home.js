a551654b9ea687f63dd2050d94da3253d9867986
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2023-11-22 10:00:40 +0000

    added hooks inputs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,101 +3,118 @@ import LoginButton from "../components/LoginButton"
 import HomeBackgroundVector from "./HomeBackgroundVector";
 import Application from "@tria-sdk/authenticate"
 import "@tria-sdk/authenticate/dist/index.css";
-import {useSignMessage,useSendTransaction,useContractWrite } from "@tria-sdk/authenticate";
+import { useSignMessage, useSendTransaction, useContractWrite } from "@tria-sdk/authenticate";
 
 
 const Home = () => {
 
-  const [showWallet, setShowWallet] = useState(false)
-useEffect(() => {
-  const item = localStorage.getItem("tria.wallet.store");
-  console.log(item);
-}, []);
+  const [showWallet, setShowWallet] = useState(false);
+  const [message, setMessage] = useState('');
+  const [amount, setAmount] = useState(0.00001);
+  const [senderAddress, setSenderAddress] = useState('');
+  const [recepientAddress, setrecepientAddress] = useState('');
+  const [contractDetails, setContractDetails] = useState('');
+  const [chainName, setChainName] = useState('POLYGON');
+  const [tokenAddress, setTokenAddress] = useState('');
+  useEffect(() => {
+    const item = localStorage.getItem("tria.wallet.store");
+    console.log(item);
+  }, []);
 
-const { data:sign, isError, isLoading, isSuccess,signMessage  } = useSignMessage({
-  message:"lalit this side ",
-  chainName:"POLYGON"
-});
+  console.log("data------------------>",{message,amount,senderAddress,recepientAddress,contractDetails,chainName,tokenAddress});
 
-const { data,sendTransaction } = useSendTransaction({
-  amount: 0.00001,
-  senderAddress: "lalitthreely@tria",
-  recepientAddress: "dev@tria",
-  chainName: "POLYGON",
-  tokenAddress:""
-});
+  const { data: sign, isError, isLoading, isSuccess, signMessage } = useSignMessage({
+    message,
+    chainName
+  });
 
-const { data :contractwrite,  write }=useContractWrite({
-    chainName: "POLYGON",
-    tokenAddress: undefined,
-    contractDetails: {
-      contractAddress: '0xd1fD14e3Cf4f96E63A1561681dc8765DF8f7Cf91',
-      abi: [
-        {
-          inputs: [
-            { internalType: 'uint256', name: '_tokenID', type: 'uint256' },
-            { internalType: 'address', name: '_claimer', type: 'address' },
-          ],
-          name: 'claimCoupon',
-          outputs: [],
-          stateMutability: 'nonpayable',
-          type: 'function',
-        },
-      ],
-      functionName: 'claimCoupon',
-      args: [1, '0x7Ae1bBCe3557D46313a960C0982637967eF5c1f7'],
-      // value: 1,
-    }});
+  const { data, sendTransaction } = useSendTransaction({
+    amount,
+    senderAddress,
+    recepientAddress,
+    chainName,
+    tokenAddress
+  });
 
-console.log("send data----------------->",data);
-console.log("sign data----------------->",sign);
-console.log("contract write---------------->",contractwrite);
+  const { data: contractwrite, write } = useContractWrite({
+    chainName,
+    tokenAddress,
+    contractDetails
+  });
 
- const handleButtonClick = () => {
-  const data = {triaName:'testName', evmAddress:'sample'};
-  localStorage.setItem("tria.wallet.store", JSON.stringify(data));
-}
+  console.log("send data----------------->", data);
+  console.log("sign data----------------->", sign);
+  console.log("contract write---------------->", contractwrite);
+
+  const handleButtonClick = () => {
+    const data = { triaName: 'testName', evmAddress: 'sample' };
+    localStorage.setItem("tria.wallet.store", JSON.stringify(data));
+  }
 
   return (
     <div className="">
-      {/* <div className="absolute top-20 left-20 "><HomeBackgroundVector/></div>
-      <div
-              className="wallet_icon fixed w-[80px] bottom-4 right-8 cursor-pointer"
-              onClick={() => {
-                setShowWallet(!showWallet);
-              }}
-            >
-              <div className="relative ">
-                <div className="left-[15.49px] top-[15.49px]">
-                  {" "}
-                  <img
-                    className="wallet_icon "
-                    src="/icons/wallet.png"
-                    alt="wallet"
-                  />
-                </div>
-              </div>
-              {showWallet && (
-              <div className="bg flex  justify-between bg-black">
-                <div className="mr-2 fixed right-2 bottom-16 rounded-[20px] overflow-hidden">
-                    <iframe width="314" height="588" className="" src="http://localhost:3000"/> 
 
-                   
-
-                </div>
-              </div>
-            )}
-            
-            </div>
-
-      */}
 
       {/* <button className="absolute top-[50px] left-[50px] w-[80px] bg-pink-500" onClick={handleButtonClick}>click here</button> */}
- <Application dappName={"Empire"} logo={"https://www.empireofsight.com/assets/images/logo-icon.svg"} dappDomain={window.parent.origin} primaryColor = "#70CA00" />
- <button className="text-white bg-blue-500  px-4 py-2 rounded-md mr-2" onClick={()=>signMessage()}>Sign Message</button>
- <button className="text-white bg-green-500  px-4 py-2 rounded-md" onClick={()=>sendTransaction()}>send Transaction</button>
- <button className="text-white bg-pink-500  px-4 py-2 rounded-md" onClick={()=>write()}>call contract</button>
+      <Application dappName={"Empire"} logo={"https://www.empireofsight.com/assets/images/logo-icon.svg"} dappDomain={window.parent.origin} primaryColor="#70CA00" />
+      <div className="flex flex-col w-[20%]">
+        {/* Common for all buttons */}
+        <input
+          type="text"
+          placeholder="Enter chain name"
+          value={chainName}
+          onChange={(e) => setChainName(e.target.value)}
+          className="border rounded-md px-2 py-1 mr-2"
+        />
+        <input
+          type="text"
+          placeholder="Enter token address"
+          value={tokenAddress}
+          onChange={(e) => setTokenAddress(e.target.value)}
+          className=" mb-16 border rounded-md px-2 py-1 mr-2"
+        />
+      <input
+        type="text"
+        placeholder="Enter message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        className="border rounded-md px-2 py-1 mr-2"
+      />
+      <button className=" mb-16 text-white bg-blue-500  px-4 py-2 rounded-md mr-2" onClick={() => signMessage()}>Sign Message</button>
+      
+      <input
+          type="number"
+          placeholder="Enter amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          className="border rounded-md px-2 py-1 mr-2"
+        />
+        <input
+          type="text"
+          placeholder="Enter sender address"
+          value={senderAddress}
+          onChange={(e) => setSenderAddress(e.target.value)}
+          className="border rounded-md px-2 py-1 mr-2"
+        />
+        <input
+          type="text"
+          placeholder="Enter recipient address"
+          value={recepientAddress}
+          onChange={(e) => setrecepientAddress(e.target.value)}
+          className="border rounded-md px-2 py-1 mr-2"
+        />
+      <button className=" mb-16 text-white bg-green-500  px-4 py-2 rounded-md" onClick={() => sendTransaction()}>send Transaction</button>
+
+      <input
+          type="text"
+          placeholder="Enter contract details"
+          value={contractDetails}
+          onChange={(e) => setContractDetails(e.target.value)}
+          className="border rounded-md px-2 py-1 mr-2"
+        />
+      <button className="text-white bg-pink-500  px-4 py-2 rounded-md" onClick={() => write()}>call contract</button>
       </div>
+    </div>
   )
 }
 
