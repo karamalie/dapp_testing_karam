9b1c5c3617dcb32fe86e7173c302830ba50fc090
LEGO-SUDO <nakulksingh3127@gmail.com>
2024-01-24 17:24:43 +0530

    wallet close on outside click

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import Home from "./pages/Home";
-import { useTriaConnector, useDisconnect } from "@tria-sdk/connect-staging";
-import { TriaConnectProvider } from "@tria-sdk/authenticate";
-import Application from "@tria-sdk/authenticate";
-import { useEffect, useState, useRef } from "react";
-import Wallet from "./pages/Wallet";
-import TriaLogin from "./components/TriaLogin";
-import { useAccount } from "wagmi";
-import ReactGA from 'react-ga';
-import Context from "./Context";
+import Home from './pages/Home'
+import { useTriaConnector, useDisconnect } from '@tria-sdk/connect-staging'
+import { TriaConnectProvider } from '@tria-sdk/authenticate'
+import Application from '@tria-sdk/authenticate'
+import { useEffect, useState, useRef } from 'react'
+import Wallet from './pages/Wallet'
+import TriaLogin from './components/TriaLogin'
+import { useAccount } from 'wagmi'
+import ReactGA from 'react-ga'
+import Context from './Context'
 // Initialize React Ga with your tracking ID
 ReactGA.initialize('G-8B9M5DSMB4', {
-  debug: true
-});
-
+  debug: true,
+})
 
 function App() {
   const { globalData } = useTriaConnector({
-    authUrl: "https://auth-tria.vercel.app",
-    walletUrl: "https://staging-tria-wallet.vercel.app",
-  });
+    authUrl: 'https://auth-tria.vercel.app',
+    walletUrl: 'https://staging-tria-wallet.vercel.app',
+  })
 
-  const [walletColor, setWalletColor] = useState("#FF4E17")
-  const [reloadFlag, setReloadFlag] = useState(false);
+  const [walletColor, setWalletColor] = useState('#FF4E17')
+  const [reloadFlag, setReloadFlag] = useState(false)
   const [launchTria, setLaunchTria] = useState(false)
   const [clicked, setClicked] = useState(false)
-  const walletRef = useRef(null);
+  const walletRef = useRef(null)
 
   const { account } = useAccount()
 
   function getWindowSize() {
-    const { innerWidth, innerHeight } = window;
-    return { innerWidth, innerHeight };
+    const { innerWidth, innerHeight } = window
+    return { innerWidth, innerHeight }
   }
 
-  const [windowSize, setWindowSize] = useState(getWindowSize());
+  const [windowSize, setWindowSize] = useState(getWindowSize())
 
   useEffect(() => {
     function handleWindowResize() {
-      setWindowSize(getWindowSize());
+      setWindowSize(getWindowSize())
     }
 
-    window.addEventListener('resize', handleWindowResize);
+    window.addEventListener('resize', handleWindowResize)
 
     return () => {
-      window.removeEventListener('resize', handleWindowResize);
-    };
-  }, []);
+      window.removeEventListener('resize', handleWindowResize)
+    }
+  }, [])
 
   // console.log(account);
 
   useEffect(() => {
-    console.log("wagmi account", account)
+    console.log('wagmi account', account)
   }, [account])
 
   const obj = {
@@ -60,61 +59,81 @@ function App() {
     setReloadFlag,
     launchTria,
     setLaunchTria,
-    clicked
+    clicked,
   }
 
   function useOutsideAlerter(ref) {
     useEffect(() => {
-
       function handleClickOutside(event) {
         if (ref.current && !ref.current.contains(event.target)) {
           setClicked(false)
         }
       }
 
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutside)
       return () => {
-
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }, [ref]);
+        document.removeEventListener('mousedown', handleClickOutside)
+      }
+    }, [ref])
   }
 
-  useOutsideAlerter(walletRef);
+  useOutsideAlerter(walletRef)
 
   useEffect(() => {
-    console.log("clicked --> ", clicked)
+    console.log('clicked --> ', clicked)
   }, [clicked])
 
   return (
     <>
       <Context.Provider value={obj}>
-        {windowSize.innerWidth < 450 && (!localStorage.getItem('tria.wallet.store') && !localStorage.getItem("wagmi.connected")) ? <div style={{ zIndex: 88 }} className="h-screen w-full absolute bg-stone-800 bg-opacity-60"></div> : null}
-        <div className="bg-black h-[100vh] w-[100vw]">
-          {localStorage.getItem('tria.wallet.store') === null && localStorage.getItem("wagmi.connected") === null ? <div className="w-full h-20 px-10 py-4 bg-neutral-900 border-b border-stone-950 justify-start items-center gap-4 inline-flex">
-            <div className="grow shrink basis-0 h-12 justify-between items-center flex">
-              <div className=" justify-center items-center gap-2.5 flex">
-                <div className=" h-7 relative">
-                  <div className="">
-                    <img src="/icons/logoName.svg" alt="tria_logo" />
+        {windowSize.innerWidth < 450 &&
+        !localStorage.getItem('tria.wallet.store') &&
+        !localStorage.getItem('wagmi.connected') ? (
+          <div
+            style={{ zIndex: 88 }}
+            className='h-screen w-full absolute bg-stone-800 bg-opacity-60'
+          ></div>
+        ) : null}
+        <div className='bg-black h-[100vh] w-[100vw]'>
+          {localStorage.getItem('tria.wallet.store') === null &&
+          localStorage.getItem('wagmi.connected') === null ? (
+            <div className='w-full h-20 px-10 py-4 bg-neutral-900 border-b border-stone-950 justify-start items-center gap-4 inline-flex'>
+              <div className='grow shrink basis-0 h-12 justify-between items-center flex'>
+                <div className=' justify-center items-center gap-2.5 flex'>
+                  <div className=' h-7 relative'>
+                    <div className=''>
+                      <img src='/icons/logoName.svg' alt='tria_logo' />
+                    </div>
                   </div>
-                </div>
-                <div className="px-2 py-1 rounded-[69px] border border-violet-400 justify-center items-center gap-2.5 flex">
-                  <div className="text-center text-violet-400 text-base font-normal font-['Neue Haas Grotesk Display Pro'] leading-tight tracking-tight">Demo</div>
+                  <div className='px-2 py-1 rounded-[69px] border border-violet-400 justify-center items-center gap-2.5 flex'>
+                    <div className="text-center text-violet-400 text-base font-normal font-['Neue Haas Grotesk Display Pro'] leading-tight tracking-tight">
+                      Demo
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div> : null}
+          ) : null}
           <TriaConnectProvider />
 
-          {localStorage.getItem('tria.wallet.store') || localStorage.getItem("wagmi.connected") ? <Home /> : null}
+          {localStorage.getItem('tria.wallet.store') ||
+          localStorage.getItem('wagmi.connected') ? (
+            <Home />
+          ) : null}
           <div ref={walletRef}>
-            <TriaLogin walletColor={walletColor} reloadFlag={reloadFlag} launchTria={launchTria} clicked={clicked} setClicked={setClicked} />
+            <TriaLogin
+              walletColor={walletColor}
+              reloadFlag={reloadFlag}
+              launchTria={launchTria}
+              setLaunchTria={setLaunchTria}
+              clicked={clicked}
+              setClicked={setClicked}
+            />
           </div>
         </div>
       </Context.Provider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/TriaLogin.js
+++ src/components/TriaLogin.js
@@ -1,66 +1,105 @@
-import Application from "@tria-sdk/authenticate";
-import { useEffect, useState } from "react";
+import Application from '@tria-sdk/authenticate'
+import { useEffect, useRef, useState } from 'react'
 
-const TriaLogin = ({ walletColor, reloadFlag, launchTria, clicked, setClicked }) => {
+const TriaLogin = ({
+  walletColor,
+  reloadFlag,
+  launchTria,
+  setLaunchTria,
+  clicked,
+  setClicked,
+}) => {
+  const walletRef = useRef()
+  const [loadAgain, setloadAgain] = useState(false)
 
-    const [loadAgain, setloadAgain] = useState(false)
+  useEffect(() => {
+    setloadAgain(true)
+    setTimeout(() => {
+      setloadAgain(false)
+    }, 10)
+  }, [reloadFlag])
 
-    useEffect(() => {
-        setloadAgain(true)
-        setTimeout(() => {
-            setloadAgain(false)
-        }, 10)
-    }, [reloadFlag]);
+  function getWindowSize() {
+    const { innerWidth, innerHeight } = window
+    return { innerWidth, innerHeight }
+  }
 
-    function getWindowSize() {
-        const { innerWidth, innerHeight } = window;
-        return { innerWidth, innerHeight };
+  const [windowSize, setWindowSize] = useState(getWindowSize())
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize())
     }
 
-    const [windowSize, setWindowSize] = useState(getWindowSize());
+    window.addEventListener('resize', handleWindowResize)
 
-    useEffect(() => {
-        function handleWindowResize() {
-            setWindowSize(getWindowSize());
-        }
+    return () => {
+      window.removeEventListener('resize', handleWindowResize)
+    }
+  }, [])
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (walletRef.current && !walletRef.current.contains(event.target)) {
+        setLaunchTria(false)
+      }
+    }
 
-        window.addEventListener('resize', handleWindowResize);
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [walletRef])
 
-        return () => {
-            window.removeEventListener('resize', handleWindowResize);
-        };
-    }, []);
+  return (
+    <div
+      onClick={() => setClicked(!clicked)}
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        zIndex: 89, // can update this to 9999 as well
+        height: 0,
+        width: 0,
+      }}
+      ref={walletRef}
+    >
+      {!loadAgain ? (
+        <Application
+          dappName={'Tria Demo'}
+          logo={'https://svgshare.com/i/11sN.svg'}
+          dappDomain={window.parent.origin}
+          primaryColor={walletColor}
+          defaultChain='MUMBAI'
+          supportedChains={[
+            'MUMBAI',
+            'POLYGON',
+            'METIS',
+            'METIS-TESTNET',
+            'MANTLE',
+            'MANTA-TESTNET',
+            'MANTA',
+            'MANTLE-TESTNET',
+            'FUSE',
+            'ETH',
+            'BINANCE',
+            'OPTIMISM',
+            'ARBITRUM',
+            'AVALANCHE',
+            'FANTOM',
+          ]}
+          uiType='yes'
+          darkMode={true}
+          buttonPosition={{ x: '0vw', y: '0vh' }}
+          walletButtonDraggable={false}
+          triaStaging={true}
+          customWalletVisible={launchTria}
+          customWalletButton={windowSize.innerWidth > 500 ? false : true}
+        />
+      ) : null}
+    </div>
+  )
+}
 
-
-    return (
-        <div
-            onClick={() => setClicked(!clicked)}
-            style={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                zIndex: 89, // can update this to 9999 as well
-                height: 0,
-                width: 0,
-            }}
-        >
-            {!loadAgain ? <Application
-                dappName={"Tria Demo"}
-                logo={"https://svgshare.com/i/11sN.svg"}
-                dappDomain={window.parent.origin}
-                primaryColor={walletColor}
-                defaultChain="MUMBAI"
-                supportedChains={["MUMBAI", "POLYGON", "METIS", "METIS-TESTNET", "MANTLE", "MANTA-TESTNET", "MANTA", "MANTLE-TESTNET", "FUSE", "ETH", "BINANCE", "OPTIMISM", "ARBITRUM", "AVALANCHE", "FANTOM"]}
-                uiType="yes"
-                darkMode={true}
-                buttonPosition={{ x: "0vw", y: "0vh" }}
-                walletButtonDraggable={false}
-                triaStaging={true}
-                customWalletVisible={launchTria}
-                customWalletButton={windowSize.innerWidth > 500 ? false : true}
-            /> : null}
-        </div>
-    );
-};
-
-export default TriaLogin;
+export default TriaLogin
