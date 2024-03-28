d6daf2f846811f9b742d64cc4175f01a911ad24b
LEGO-SUDO <nakulksingh3127@gmail.com>
2024-03-28 12:30:08 +0530

    authenticate version upgrade

--- src/components/TriaLogin.js
+++ src/components/TriaLogin.js
@@ -1,5 +1,5 @@
-import Application from "@tria-sdk/authenticate";
-import { useEffect, useRef, useState } from "react";
+import Application from "@tria-sdk/authenticate"
+import { useEffect, useRef, useState } from "react"
 
 const TriaLogin = ({
   walletColor,
@@ -9,48 +9,48 @@ const TriaLogin = ({
   clicked,
   setClicked,
 }) => {
-  const walletRef = useRef();
-  const [loadAgain, setloadAgain] = useState(false);
+  const walletRef = useRef()
+  const [loadAgain, setloadAgain] = useState(false)
 
   useEffect(() => {
-    setloadAgain(true);
+    setloadAgain(true)
     setTimeout(() => {
-      setloadAgain(false);
-    }, 10);
-  }, [reloadFlag]);
+      setloadAgain(false)
+    }, 10)
+  }, [reloadFlag])
 
   function getWindowSize() {
-    const { innerWidth, innerHeight } = window;
-    return { innerWidth, innerHeight };
+    const { innerWidth, innerHeight } = window
+    return { innerWidth, innerHeight }
   }
 
-  const [windowSize, setWindowSize] = useState(getWindowSize());
+  const [windowSize, setWindowSize] = useState(getWindowSize())
 
   useEffect(() => {
     function handleWindowResize() {
-      setWindowSize(getWindowSize());
+      setWindowSize(getWindowSize())
     }
 
-    window.addEventListener("resize", handleWindowResize);
+    window.addEventListener("resize", handleWindowResize)
 
     return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
+      window.removeEventListener("resize", handleWindowResize)
+    }
+  }, [])
   useEffect(() => {
     function handleClickOutside(event) {
       if (walletRef.current && !walletRef.current.contains(event.target)) {
-        setLaunchTria(false);
+        setLaunchTria(false)
       }
     }
 
     // Bind the event listener
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside)
     return () => {
       // Unbind the event listener on clean up
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [walletRef]);
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [walletRef])
 
   return (
     <div
@@ -71,7 +71,7 @@ const TriaLogin = ({
           logo={"https://svgshare.com/i/11sN.svg"}
           dappDomain={window.parent.origin}
           primaryColor={walletColor}
-          defaultChain="MUMBAI"
+          defaultChain='MUMBAI'
           supportedChains={[
             "MUMBAI",
             "POLYGON",
@@ -90,21 +90,22 @@ const TriaLogin = ({
             "FANTOM",
             "SOLANA",
           ]}
-          uiType="yes"
-          litStaging={true}
+          uiType='yes'
+          triaEnvironment='staging'
+          walletEnvironment='staging'
           darkMode={true}
           buttonPosition={{ x: "0vw", y: "0vh" }}
           walletButtonDraggable={false}
           customWalletVisible={launchTria}
           customWalletButton={windowSize.innerWidth > 500 ? false : true}
-          analyticsKeys = {{
+          analyticsKeys={{
             clientId: "397f3be4-c28f-4ce0-9c05-1afc1f8fa959",
-            projectId: "f2ba4e6a-314e-4280-a221-2efe049e5fe1"
+            projectId: "f2ba4e6a-314e-4280-a221-2efe049e5fe1",
           }}
         />
       ) : null}
     </div>
-  );
-};
+  )
+}
 
-export default TriaLogin;
+export default TriaLogin
